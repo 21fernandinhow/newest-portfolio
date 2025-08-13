3c102171815f192ebece7a880c6615c95fe564dc
Fernando Carvalho <21fernandinhow@gmail.com>
2025-08-13 14:33:50 -0300

    Revert "improve loading time using lazy loading"
    This reverts commit 3814e86a2207759fcc1992db1023ff92126875ac.

--- src/components/Samantha.tsx
+++ src/components/Samantha.tsx
@@ -1,24 +1,35 @@
-import { OrbitControls } from "@react-three/drei";
+import { useRef } from "react";
 import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
-import { SamanthaModel } from "./SamanthaModel";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import { Group } from "three";
 
-export const Samantha = () => (
-    <div id="samantha">
-        <Canvas camera={{ position: [0, 0, 2] }} >
-            <ambientLight intensity={0.6} />
-            <directionalLight position={[5, 5, 5]} />
-            <OrbitControls
-                enableZoom={false}
-                enablePan={false}
-                minPolarAngle={Math.PI / 2}
-                maxPolarAngle={Math.PI / 2}
-                minAzimuthAngle={-Math.PI / 3}
-                maxAzimuthAngle={Math.PI / 3}
-            />
-            <Suspense fallback={null}>
-                <SamanthaModel />
-            </Suspense>
-        </Canvas>
-    </div>
-);
+export const Samantha = () => {
+    const group = useRef<Group | null>(null);
+    const { scene } = useGLTF("/samantha.glb");
+
+    return (
+        <div id="samantha">
+            <Canvas camera={{ position: [0, 0, 2] }} >
+                <ambientLight intensity={0.6} />
+                <directionalLight position={[5, 5, 5]} />
+                <OrbitControls
+                    enableZoom={false}
+                    enablePan={false}
+                    minPolarAngle={Math.PI / 2}
+                    maxPolarAngle={Math.PI / 2}
+                    minAzimuthAngle={-Math.PI / 3}
+                    maxAzimuthAngle={Math.PI / 3}
+                />
+                <group
+                    ref={group}
+                    dispose={null}
+                    position={[-0.25, -0.25, 0.25]}
+                    scale={[1.2, 1.2, 1.2]}
+                    rotation={[0, Math.PI / 6, 0]}
+                >
+                    <primitive object={scene} />
+                </group>
+            </Canvas>
+        </div>
+    );
+};
