d2b191a0ef888340753a32aaa38341ed72376186
Fernando Carvalho <21fernandinhow@gmail.com>
2025-08-13 13:06:23 -0300

    improve translations

--- src/translations/translations.ts
+++ src/translations/translations.ts
@@ -2,23 +2,23 @@ export const translations = {
   pt: {
     samantha: {
       initialMessage:
-        "Oi! Eu sou a Samantha, inteligência artificial criada pelo Fernando Carvalho. Estou aqui para contar tudo sobre o trabalho dele como desenvolvedor FullStack e mostrar seus projetos. Arraste para o lado para navegar ➡️",
+        "Oi! Eu sou a Samantha, inteligência artificial criada pelo Fernando Carvalho. Estou aqui para contar tudo sobre o trabalho dele como desenvolvedor FullStack e mostrar seus projetos. Fique a vontade para fazer perguntas e arraste para o lado para navegar ➡️",
     },
     contact: {
       title: "Contato",
       description: "Aqui estão as principais formas de se conectar comigo:",
     },
-    typeHere: "Digite aqui sua mensagem...",
+    typeHere: "Faça uma pergunta a Samantha...",
   },
   en: {
     samantha: {
       initialMessage:
-        "Hi! I'm Samantha, an artificial intelligence created by Fernando Carvalho. I'm here to tell you all about his work as a FullStack developer and show his projects. Swipe to the side to navigate ➡️",
+        "Hi! I'm Samantha, an artificial intelligence created by Fernando Carvalho. I'm here to tell you all about his work as a FullStack developer and show his projects. Ask any questions you want and swipe to the side to navigate ➡️",
     },
     contact: {
       title: "Contact",
       description: "Here are the main ways to get in touch with me:",
     },
-    typeHere: "Type here your messsage...",
+    typeHere: "Ask Samantha...",
   },
 };
