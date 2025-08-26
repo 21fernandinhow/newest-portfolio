3036b4f0ce49d4cd7abc2b65695b17f06c89251c
Fernando Carvalho <21fernandinhow@gmail.com>
2025-08-26 15:34:45 -0300

    chat automatic scroll on new message

--- src/components/ChatDrawer.tsx
+++ src/components/ChatDrawer.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { useLanguage } from "../context/LanguageContext";
 import { useMessages } from "../context/MessagesContext";
 import { MessageInput } from "./MessageInput";
@@ -11,9 +12,24 @@ export const ChatDrawer = ({ isOpen }: ChatDrawerProps) => {
     const { translation } = useLanguage()
     const { messages } = useMessages()
 
+    const messagesContainerRef = useRef<HTMLDivElement | null>(null);
+
+    const scrollToBottom = () => {
+        if (messagesContainerRef.current) {
+            messagesContainerRef.current.scrollTo({
+                top: messagesContainerRef.current.scrollHeight,
+                behavior: 'smooth',
+            });
+        }
+    };
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [messages]);
+
     return (
         <div className={`chat-drawer ${isOpen ? "open" : ""}`}>
-            <div className="chat-body">
+            <div className="chat-body" ref={messagesContainerRef}>
                 {messages.filter((msg) => msg.role !== "system").map((msg, index) => (
                     <div
                         key={index}
